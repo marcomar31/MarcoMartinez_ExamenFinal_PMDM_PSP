f9b92eb43d08458ec24a9a43e6d4ff0702a22cf8
Marco Martínez <129172352+marcomar31@users.noreply.github.com>
2024-02-29 18:03:50 +0100

    Commit 3: Añadidos TextFormField y Botones a LoginView.

--- marcomartinez_examenfinal/lib/OnBoarding/LoginView.dart
+++ marcomartinez_examenfinal/lib/OnBoarding/LoginView.dart
@@ -5,8 +5,51 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Column column = const Column(
+    TextEditingController tecEmail = TextEditingController();
+    TextEditingController tecPassword = TextEditingController();
+
+    Column column = Column(
       children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+          child: TextFormField(
+            controller: tecEmail,
+            style: const TextStyle(color: Colors.white),
+            decoration: const InputDecoration(
+              labelText: "Email",
+              labelStyle: TextStyle(color: Colors.white),
+            ),
+            cursorColor: const Color.fromRGBO(22, 36, 71, 1),
+          ),
+
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 16),
+          child: TextFormField(
+            controller: tecPassword,
+            style: const TextStyle(color: Colors.white),
+            decoration: const InputDecoration(
+              labelText: "Password",
+              labelStyle: TextStyle(color: Colors.white),
+            ),
+            cursorColor: const Color.fromRGBO(35, 41, 49, 1),
+            obscureText: true,
+          ),
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              child: TextButton(onPressed: () {}, child: const Text("ACEPTAR")),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              child: TextButton(onPressed: () {}, child: const Text("REGISTRARSE")),
+            ),
+          ],
+        ),
       ],
     );
 
