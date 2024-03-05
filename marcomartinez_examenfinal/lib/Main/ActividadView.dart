69a9d26a62359aeb9aa866d9e31529d5c1524487
Marco Martínez <129172352+marcomar31@users.noreply.github.com>
2024-03-05 02:06:40 +0100

    Commit 49: Cambios visuales en ActividadView para futura adaptación a Web.

--- marcomartinez_examenfinal/lib/Main/ActividadView.dart
+++ marcomartinez_examenfinal/lib/Main/ActividadView.dart
@@ -15,8 +15,6 @@ class _ActividadViewState extends State<ActividadView> {
 
   @override
   Widget build(BuildContext context) {
-    double anchoImagen = PlatformAdmin.getScreenWidth(context)-40;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -37,97 +35,102 @@ class _ActividadViewState extends State<ActividadView> {
         iconTheme: const IconThemeData(color: Colors.white),
       ),
       backgroundColor: const Color.fromRGBO(10, 35, 65, 1.0),
-      body: SingleChildScrollView(
+      body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: anchoImagen,
-                height: anchoImagen,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(20),
-                  image: actividad!.imagenUrl.isNotEmpty
-                      ? DecorationImage(
-                    image: NetworkImage(actividad!.imagenUrl),
-                    fit: BoxFit.cover,
-                  )
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (actividad?.imagenUrl != "")
+                Container(
+                  width: PlatformAdmin.isWebPlatform() ? 600 : PlatformAdmin.getScreenWidth(context)-40,
+                  height: PlatformAdmin.isWebPlatform() ? 600 : PlatformAdmin.getScreenWidth(context)-40,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(20),
+                    image: actividad!.imagenUrl.isNotEmpty
+                        ? DecorationImage(
+                      image: NetworkImage(actividad!.imagenUrl),
+                      fit: BoxFit.cover,
+                    )
+                        : null,
+                  ),
+                  child: actividad!.imagenUrl.isEmpty
+                      ? Icon(Icons.image, size: PlatformAdmin.isWebPlatform() ? 600 : PlatformAdmin.getScreenWidth(context)-40/2, color: Colors.white)
                       : null,
                 ),
-                child: actividad!.imagenUrl.isEmpty
-                    ? Icon(Icons.image, size: anchoImagen/2, color: Colors.white)
-                    : null,
-              ),
-              const SizedBox(height: 30,),
-              const Text(
-                'Descripción:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                actividad?.descripcion ?? "",
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Fecha:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                "${actividad?.fecha.day.toString().padLeft(2, '0')}/${actividad?.fecha.month.toString().padLeft(2, '0')}/${actividad?.fecha.year.toString()}",
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Precio:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                "${actividad?.precio.toString() ?? ""} €",
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-              ),
-              if (actividad?.geoloc != null)
-              const SizedBox(height: 20),
-              if (actividad?.geoloc != null)
+                if (actividad?.imagenUrl != "")
+                const SizedBox(height: 30,),
                 const Text(
-                'Ubicación:',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+                  'Descripción:',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              if (actividad?.geoloc != null)
                 Text(
-                "${actividad?.geoloc?.longitude ?? ""}, ${actividad?.geoloc?.latitude ?? ""}",
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
+                  actividad?.descripcion ?? "",
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 20),
+                const Text(
+                  'Fecha:',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "${actividad?.fecha.day.toString().padLeft(2, '0')}/${actividad?.fecha.month.toString().padLeft(2, '0')}/${actividad?.fecha.year.toString()}",
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Precio:',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "${actividad?.precio.toString() ?? ""} €",
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                if (actividad?.geoloc != null)
+                const SizedBox(height: 20),
+                if (actividad?.geoloc != null)
+                  const Text(
+                  'Ubicación:',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                if (actividad?.geoloc != null)
+                  Text(
+                  "${actividad?.geoloc?.longitude ?? ""}, ${actividad?.geoloc?.latitude ?? ""}",
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 30,),
+              ],
+            ),
           ),
         ),
       ),
